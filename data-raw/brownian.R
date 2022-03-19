26b50620d520e4a27867cd0ec7859a587212c30e
StevenGolovkine <steven_golovkine@icloud.com>
2022-03-19 17:10:02 +0000

    Brownian simulation with H=0.5

--- data-raw/brownian.R
+++ data-raw/brownian.R
@@ -1,6 +1,6 @@
 # data-raw/brownian.R
 # Data import and processing pipeline
 
-brownian <- readr::read_csv("./data-raw/brownian.csv", col_names = FALSE)
+brownian <- readr::read_csv("./data-raw/brownian.csv", col_names = TRUE)
 
 usethis::use_data(brownian, overwrite = TRUE)
